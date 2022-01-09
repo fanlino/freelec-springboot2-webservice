d7166b121e5b0cc95150a3b74b9af6ff9e8bd54e
fanlino <fanlino@gmail.com>
2022-01-09 20:55:22 +0900

    등록기능 구현

---
+++ src/main/java/kr/dhkim/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package kr.dhkim.book.springboot.service.posts;
+
+import kr.dhkim.book.springboot.domain.posts.PostsRepository;
+import kr.dhkim.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
