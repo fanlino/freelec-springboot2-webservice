98779b0382f2d80a2e4e7d8de6216b9214ff69d9
fanlino <fanlino@gmail.com>
2022-01-09 20:03:40 +0900

    HelloController 테스트

--- src/main/java/kr/dhkim/book/springboot/Application.java
+++ src/main/java/kr/dhkim/book/springboot/Application.java
@@ -1,13 +1,13 @@
-package kr.dhkim.freelecspringboot2webservice;
+package kr.dhkim.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FreelecSpringboot2WebserviceApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(FreelecSpringboot2WebserviceApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- src/test/java/kr/dhkim/book/springboot/ApplicationTests.java
+++ src/test/java/kr/dhkim/book/springboot/ApplicationTests.java
@@ -1,10 +1,10 @@
-package kr.dhkim.freelecspringboot2webservice;
+package kr.dhkim.book.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FreelecSpringboot2WebserviceApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
