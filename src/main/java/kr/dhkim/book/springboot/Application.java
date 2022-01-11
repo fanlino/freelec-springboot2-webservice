e37063babc9d9e1c6b25251991a4c73442522ea3
fanlino <fanlino@gmail.com>
2022-01-11 09:11:22 +0900

    jpa audting 추가 index.mustache 추가

--- src/main/java/kr/dhkim/book/springboot/Application.java
+++ src/main/java/kr/dhkim/book/springboot/Application.java
@@ -2,7 +2,9 @@ package kr.dhkim.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
 
---
+++ src/main/java/kr/dhkim/book/springboot/web/dto/IndexController.java
@@ -0,0 +1,12 @@
+package kr.dhkim.book.springboot.web.dto;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
